84b42c8607c7a0805f5c765db3e239c82816abf2
himanshu1223344 <165650973+himanshu1223344@users.noreply.github.com>
2026-03-01 15:15:12 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+import threading
+from bot import main as bot_main
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "GMB Ranking Tracker is Running ✅"
+
+def run_bot():
+    bot_main()
+
+# Start bot in background thread
+t = threading.Thread(target=run_bot)
+t.daemon = True
+t.start()
 
 if __name__ == '__main__':
     app.run()
